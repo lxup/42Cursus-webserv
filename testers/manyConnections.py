d5e9b108a55e1d8e8e04a87237a8ebf7514efc59
toukoum <raphaelgiraud12@gmail.com>
2024-08-05 15:25:51 +0200

    adding readme and testers

--- testers/manyConnections.py
+++ testers/manyConnections.py
@@ -13,17 +13,29 @@ def make_request(url, index):
         duration = time.time() - start_time
         print(f"Thread {index}: Request failed: {e}, Time taken: {duration:.4f} seconds")
 
+#avec les thread ca marche pas ahaha
+# def main(url, num_clients):
+#     threads = []
+#     total_start_time = time.time()
+    
+#     for i in range(num_clients):
+#         thread = threading.Thread(target=make_request, args=(url, i))
+#         threads.append(thread)
+#         thread.start()
+#       #   time.sleep(0.001)
+
+#     for thread in threads:
+#         thread.join()
+    
+#     total_duration = time.time() - total_start_time
+#     print(f"\nTotal time taken: {total_duration:.4f} seconds")
+
 def main(url, num_clients):
     threads = []
     total_start_time = time.time()
     
     for i in range(num_clients):
-        thread = threading.Thread(target=make_request, args=(url, i))
-        threads.append(thread)
-        thread.start()
-
-    for thread in threads:
-        thread.join()
+        make_request(url, i)
     
     total_duration = time.time() - total_start_time
     print(f"\nTotal time taken: {total_duration:.4f} seconds")
